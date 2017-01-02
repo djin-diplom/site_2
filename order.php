c3160dfd4860e9b7b1c913bc433747dec317e95d
21vek <git@github.com>
2017-01-02 12:37:20 +0300

    1

--- order.php
+++ order.php
@@ -22,53 +22,51 @@ require("header.php");
             <p class='tit_work'>Заказать работу</p>
             <div class='forma_div'>
                 <strong>Внимание!</strong> Поля отмеченные звездочкой ( * ) являются <strong>обязательными</strong> для заполнения!<br>
-                <form action='order.php' method='post' enctype='multipart/form-data'>
-                    <p><strong>ФИО (или просто имя): *</strong></p>
-                    <input type='text' class='input-text' name='name'>
-                    <p><strong>Контактный телефон: *</strong></p>
-                    <input type='text' class='input-text' name='phone'>
-                    <p><strong>E-mail: *</strong></p>
-                    <input type='text' class='input-text' name='email'>
-                    <p><strong>ICQ</strong></p>
-                    <input type='text' class='input-text' name='icq'>
-                    <p><strong>ВУЗ</strong></p>
-                    <input type='text' class='input-text' name='uni'>
-                    <p><strong>Тип работы (диплом, курсовая, контрольная, отчет по практике, другое): *</strong></p>
-                    <input type='text' class='input-text' name='type'>
-                    <p><strong>Тема работы: *</strong></p>
-                    <input type='text' class='input-text' name='item'>
-                    <p><strong>Количество страниц: *</strong></p>
-                    <input type='text' class='input-text' name='pages'>
-                    <p><strong>Срок выполнения (дни, месяцы): *</strong></p>
-                    <input type='text' class='input-text' name='time'>
-                    <p><strong>Название предприятия - если работа носит практический характер:</strong></p>
-                    <input type='text' class='input-text' name='org'>
-                    <p><strong>Прикрепить методическое пособие, дополнительные материалы:</strong></p>
-                    <p><input type='file' name='userfile[]' class='input-text'></p><p><input type='file' name='userfile[]' class='input-text'></p><p><input type='file' name='userfile[]' class='input-text'></p>
-                    <br>
 
-                    <br><br>
-                    <input type='checkbox' name='yes'> <strong>Соглашаюсь с публичной офертой *</strong><br>
-                    <p class='ofer'>Публичная оферта: "Настоящим заказом я утверждаю условия оформления моей работы,
-                        которые не могут быть изменены в устной форме после подписания договора (дополнения по телефону не принимаются).
-                        В процессе телефонных переговоров я обязуюсь правильно называть свой номер договора.
-                        Я поставлен в известность и полностью согласен с тем, что если структура работы не согласована до начала
-                        работы и не приложена с моей подписью к данному бланку заказа, то доработки по изменению структуры не
-                        осуществляются, так как специфика оказания информационных услуг предусматривает различные мнения по написанию одной
-                        и той же работы. Для дипломных проектов в связи с этим в течение 5 дней после подписания договора я обязуюсь
-                        согласовать со своим руководителем дипломной работы структуру написания будущей дипломной работы и предоставить
-                        сотруднику &laquo;<?php echo $main_name; ?>&raquo; два экземпляра (один остается у Заказчика, другой у Подрядчика).
-                        Кроме того, по дипломным проектам я обязуюсь согласовать второстепенные моменты – а именно: методики, выборки,
-                        гипотезы и задачи, до того момента, как к работе приступят. Если же заранее это не согласовано, то я не буду иметь
-                        претензий по не мною выбранным методикам, гипотезам и задачам. По балансам и отчетам прибылях и убытков ситуация
-                        аналогична. Я согласен с тем, что предоплата, вносимая мною, не возвращается, так как к работе приступают в
-                        тот же день. Остальные замечания по доработке должны быть предоставлены мною в письменном виде и разборчивым
-                        подчерком на бланке доработки (исправления вносимые преподавателем на самой работе не принимаются). Работу
-                        Подрядчик при выполнении всех вышеперечисленных условий дорабатывает в течение 10 дней с момента передачи бланка
-                        доработки (более укороченные сроки возможны только по обоюдной договоренности Сторон). Я поставлен в известность
-                        что этапы работы над информационным проектом предусматривает этап доработок (не менее 3-х доработок которые
-                        осуществляются бесплатно в рамках первоначальной договоренности сторон) и своей подписью заверяю то, что не буду
-                        иметь претензий по этому вопросу.</p><br>
+
+                    <form method="post" enctype="multipart/form-data" action="order2.php">
+
+
+
+                        <p><strong>Тип работы:*</strong></p>
+                        <select name="work_kind" id="id_work_kind" class='input-text'>
+                            <option value="" selected="selected">---</option>
+                            <option value="kontrolnaia">Контрольная работа</option>
+                            <option value="referat">Реферат</option>
+                            <option value="esse">Эссе</option>
+                            <option value="kursovaia">Курсовая работа</option>
+                            <option value="otchot">Отчет по практике</option>
+                            <option value="diploma">Диплом</option>
+                            <option value="magister_dissertation">Магистерская диссертация</option>
+                            <option value="drawings">Чертежи</option>
+                            <option value="other">Прочее</option>
+                            <option value="complex">Комплексный заказ</option>
+                        </select>
+                        <p><strong>Тема работы:*</strong></p>
+                        <textarea id="id_subject" style="width: 80%;" rows="3" cols="10" name="tema" class='input-text'></textarea>
+                        <p><strong>Предмет:</strong></p>
+                        <input id="id_predmet" style="width: 50%;" type="text" name="predmet" maxlength="100" class='input-text'>
+                        <p><strong>Учебное заведение:</strong></p>
+                        <input id="id_university" style="width: 50%;" type="text" name="university" maxlength="100" class='input-text'>
+                        <p><strong>Доп. информация<br>(объем, уникальность):</strong></p>
+                        <textarea id="id_add_info" style="width: 80%;" rows="3" cols="10" name="add_info" class='input-text'></textarea>
+                        <p><strong>Ваше имя:*</strong></p>
+                        <input style="width: 50%;" type="text" name="name" id="id_name" class='input-text'>
+                        <p><strong>E-mail:*</strong></p>
+                        <input style="width: 50%;" type="text" name="email_stud" id="id_email" class='input-text'>
+                        <p><?php
+                        $a = rand(1,10);
+                        $b = rand(1,100);
+                        $c = $a+$b;
+                        echo $a."+".$b."=";
+                        ?>*</p>
+                        <input style="width: 50%;" type="text" name="count_1" class='input-text'>
+                        <input style="width: 50%; display: none;" type="text" value=<?php echo $c?> name="count_2"><br>
+
+                        <br><br>
+
+
+
                     <input type='image' src='img/save_but.png' value='Сохранить'>
                 </form>
             </div>
@@ -76,7 +74,7 @@ require("header.php");
         </div>
 
 
-<?php
+        <?php
     require("footer.php");
     ?>
 </body>
---
+++ order2.php
@@ -0,0 +1,121 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru-ru" lang="ru-ru">
+<head>
+    <?php
+    require("head.php");
+    ?>
+    <title>Заказать работу</title>
+
+
+</head>
+
+
+<body>
+
+
+<?php
+require("header.php");
+?>
+<tr>
+    <td class='work_zone' colspan='2'>
+        <div class='work_div'>
+
+
+            <?php
+            //Если форма отправлена
+            if(isset($_POST['submit'])) {
+
+
+                //Проверка суммы
+                if(trim($_POST['count_1']) != $_POST['count_2']) {
+                    $hasError = true;
+                }
+                //Проверка тип работы
+                if(trim($_POST['work_kind']) == '') {
+                    $hasError = true;
+                } else {
+                    $work_kind = trim($_POST['work_kind']);
+                }
+                //Проверка поля email
+                if(trim($_POST['email_stud']) == '') {
+                    $hasError = true;
+                } else {
+                    $email_stud= $_POST['email_stud'];
+                }
+                if(trim($_POST['name']) == '') {
+                    $hasError = true;
+                } else {
+                    $name= $_POST['name'];
+                }
+                if(trim($_POST['tema']) == '') {
+                    $hasError = true;
+                } else {
+                    $tema= $_POST['tema'];
+                }
+                if(trim($_POST['predmet']) == '') {
+                    $predmet= "пусто";
+                } else {
+                    $predmet= $_POST['predmet'];
+                }
+                if(trim($_POST['university']) == '') {
+                    $university= "пусто";
+                } else {
+                    $university= $_POST['university'];
+                }
+                if(trim($_POST['add_info']) == '') {
+                    $add_info= "пусто";
+                } else {
+                    $add_info= $_POST['add_info'];
+                }
+
+
+                //Если ошибок нет, отправить email
+                if (!isset($hasError)) {
+                    //$body = "
+                    //name: $name \n\n
+                    //email_stud: $email_stud \n\n
+                    //work_kind: $work_kind \n\n
+                    //tema: $tema \n\n
+                    //predmet : $predmet \n\n
+                    //university: $university \n\n
+                    //add_info: $add_info \n\n
+                    //";
+                    $body = "
+		<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+		Имя заказчика: $name <br>
+		Почта заказчика: $email_stud <br>
+		Вид работы: $work_kind <br>
+		Тема работы: $tema <br>
+		Предмет : $predmet <br>
+		Университет: $university <br>
+		Дополнительная информация: $add_info <br>
+		</body>
+</html>
+		";
+
+                    $headers  = 'MIME-Version: 1.0' . "\r\n";
+                    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+                    $headers .= 'From: '.$email. "\r\n" . 'Reply-To: ' . $email_stud;
+                    $subject = "Письмо от клиента ".$name;
+
+                    mail($email2, $subject, $body, $headers);
+                    $emailSent = true;
+
+                }
+                include("win.php");
+            }
+
+            ?>
+        </div>
+        </section>
+        </div>
+        </div>
+
+        <?php
+        require("footer.php");
+        ?>
+</body>
+</html>
