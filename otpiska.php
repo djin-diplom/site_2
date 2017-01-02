033a28f876b2563b3ac0935d483617a02631f5b8
21vek <git@github.com>
2017-01-02 09:39:58 +0300

    1

---
+++ otpiska.php
@@ -0,0 +1,29 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru-ru" lang="ru-ru">
+<head>
+    <?php
+    require("head.php");
+    ?>
+    <title>Отписка</title>
+
+
+</head>
+
+
+<body>
+
+
+<?php
+require("header.php");
+?>
+<tr>
+    <td class='work_zone' colspan='2'>
+        <div class='work_div'>
+            <p class='tit_work'>Вы успешно отписались от рассылок!</p>
+          
+        </div>
+
+        <?php
+        require("footer.php");
+        ?>
+</body>
+</html>
